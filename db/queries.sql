667fed9f694bf47cfdff101b25f858cd701428c3
Stew Sabatino <stewsabatino@gmail.com>
2021-10-28 16:54:16 -0500

    queries for select all

---
+++ db/queries.sql
@@ -0,0 +1,16 @@
+-- view all departments
+SELECT * FROM department
+-- view all roles
+SELECT * FROM roles
+-- view all employees
+SELECT * FROM employee
+-- add a department
+
+-- add a role
+
+-- add an employee
+
+-- update an employee role
+
+-- delete employee
+
